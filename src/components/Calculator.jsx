0f00c794904e9188fbf507c2500f07d00125f4cd
etoyin <toyinadesina60@gmail.com>
2024-07-22 13:15:16 +0100

    bold fields

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -48,9 +48,7 @@ function Calculator() {
             daily_power: Math.round((((load / 1000) * (day_hours / number_of_appliances)) + Number.EPSILON) * 100) / 100,
             night_power: Math.round((((load / 1000) * (night_hours / number_of_appliances)) + Number.EPSILON) * 100) / 100
         })
-    }, [rerenderMonitor])
-    
-    function isNumber(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); } 
+    }, [rerenderMonitor]);
 
     const handleChange = (i, e) => {
         let fields = appData.fields
@@ -117,23 +115,56 @@ function Calculator() {
       <div className="p-4 sm:p-10 font-quicksand">
         <div className="flex flex-wrap">
             <div className="w-full p-2 sm:p-5 md:w-7/12 border border-gray-100 rounded-lg shadow-xl my-5 md:mr-10  bg-[#f1f2f4]">
-                <div className="flex p-2 text-xs text-[#fe7029]">
-                    <div className="w-3/12">Appliances</div>
-                    <div className="w-3/12">Quantity</div>
-                    <div className="w-2/12">Load</div>
-                    <div className="w-2/12">Daytime Operating Hours</div>
-                    <div className="w-2/12">Night-time Operating Hours</div>
+                <div className="sm:flex my-2">
+                    <div className="mx-1">
+                        <textarea
+                            value="Appliances"
+                            disabled={true}
+                            className="no-scrollbar border-none text-[#fe7029] w-full h-6 resize-none sm:w-[130px] p-2 text-xs"/>
+                    </div>
+                    <div className="mx-1">
+                        <textarea 
+                            value='Quantity' 
+                            disabled={true}
+                            placeholder='Quantity'
+                            className="no-scrollbar border-none text-[#fe7029] w-full h-6 resize-none p-2 text-xs"/>
+                    </div>
+                    <div className="mx-1">
+                        <textarea
+                            value='Load'
+                            placeholder="load"
+                            disabled={true}
+                            className="no-scrollbar border-none text-[#fe7029] w-full h-6 resize-none p-2 text-xs "/>
+                    </div>
+                    <div className="mx-1">
+                        <textarea
+                            value='Daytime Operating Hours'
+                            placeholder="Daytime Operating hours"
+                            disabled={true}
+                            className="no-scrollbar border-none text-[#fe7029] h-6 sm:h-[60px] w-full resize-none p-2 text-xs"/>
+                    </div>
+                    <div className="mx-1">
+                        <textarea
+                            value='Night-time Operating Hours'
+                            placeholder="Night-time Operating hours"
+                            disabled={true}
+                            className="no-scrollbar border-none resize-none h-6 sm:h-[60px] text-[#fe7029] w-full p-2 text-xs"/>
+                    </div>
+                    <div 
+                        className="flex items-center cursor-pointer">
+                        <i className="las la-trash-alt invisible"></i>
+                    </div>
                 </div>
                 {
                     appData.fields.map((each, i) => {
                         return (
                             <div key={i} className="sm:flex my-2">
                                 <div className="mx-1">
-                                    <select id="" 
+                                    <select
                                         onChange={(e) => handleChange(i, e)}
                                         name='appliance'
                                         value={each.appliance}
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full sm:w-[130px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                         <option value="">Choose an Appliance</option>
                                         {Ratings.ratings.map((option, j) => {
                                             return (
@@ -198,22 +229,22 @@ function Calculator() {
                     <div className="bg-[#1994cb] text-base shadow py-3 px-5 mr-5 md:mr-3 lg:mr-5 rounded-full">Total Load Capacity</div>
                     
                     <div 
-                        className="text-sm shadow-lg rounded-full text-center w-20 h-20 border-8 border-[#f1f2f4] bg-[#1994cb] flex items-center justify-center">
+                        className="text-sm sm:text-base font-bold shadow-lg rounded-full text-center w-20 h-20 sm:w-24 sm:h-24 border-8 border-[#f1f2f4] bg-[#1994cb] flex items-center justify-center">
                         {appData.load}kW
                     </div>
                 </div>
                 <div className="my-3 font-semibold flex text-white flex-wrap justify-between items-center">
                     <div className="flex mb-1 items-center w-[220px] sm:w-max m-auto justify-between">
-                        <div className="bg-[#1994cb] text-[10px] shadow py-2 px-1 mr-1 rounded-full">Daily Power Consumption</div>
+                        <div className="bg-[#1994cb] text-[10px] shadow py-2 px-1 mr-1 font-bold rounded-full">Daily Power Consumption</div>
                         <div 
-                            className="text-[10px] shadow-lg rounded-full text-center w-[50px] h-[50px] border-4 border-[#f1f2f4] bg-[#1994cb] flex items-center justify-center">
+                            className="text-[10px] sm:text-[12px] font-bold shadow-lg rounded-full text-center w-[50px] h-[50px] lg:w-[60px] lg:h-[60px] border-4 border-[#f1f2f4] bg-[#1994cb] flex items-center justify-center">
                             {isNaN(appData.daily_power)? "0": appData.daily_power} kW
                         </div>
                     </div>
                     <div className="flex mb-1 items-center w-[220px] m-auto sm:w-max justify-between">
-                        <div className="bg-[#1994cb] text-[10px] shadow py-2 px-1 mr-1 rounded-full">Night-time Backup Needed</div>
+                        <div className="bg-[#1994cb] text-[10px] shadow py-2 px-1 mr-1 font-bold rounded-full">Night-time Backup Needed</div>
                         <div 
-                            className="text-[10px] shadow-lg rounded-full text-center w-[50px] h-[50px] border-4 border-[#f1f2f4] bg-[#1994cb] flex items-center justify-center">
+                            className="text-[10px] sm:text-[12px] font-bold shadow-lg rounded-full text-center w-[50px] h-[50px] lg:w-[60px] lg:h-[60px] border-4 border-[#f1f2f4] bg-[#1994cb] flex items-center justify-center">
                             {isNaN(appData.night_power)? "0" : appData.night_power} kW
                         </div>
                     </div>
@@ -227,7 +258,7 @@ function Calculator() {
                     </p>
                     <div className="flex flex-wrap items-center justify-between">
                         <div className="w-full sm:w-[150px]">
-                            <div className="rounded-full p-3 sm:p-0 sm:px-3 bg-[#f0f1f7] text-center text-[10px] shadow-inner border ">
+                            <div className="rounded-full font-bold p-3 sm:p-0 sm:px-3 bg-[#f0f1f7] text-center text-[10px] shadow-inner border ">
                                 How many hours on average everyday do you have grid electricity in your location?
                             </div>
                             <p className="text-sm flex justify-center">
@@ -244,25 +275,25 @@ function Calculator() {
                         </div>
                         <div className="w-full sm:w-7/12 text-white">
                             <div className="flex items-center w-[220px] m-auto justify-between my-1">
-                                <div className="bg-[#fe7532] text-[9px] shadow py-2 px-1 mr-1 rounded-full">Required Daily Power Generation</div>
+                                <div className="bg-[#fe7532] text-[9px] font-bold shadow py-2 px-1 mr-1 rounded-full">Required Daily Power Generation</div>
                                 <div 
-                                    className="text-[10px] shadow-lg rounded-full text-center w-[50px] h-[50px] border-4 border-[#f1f2f4] bg-[#fe7532] flex items-center justify-center">
+                                    className="text-[10px] sm:text-[12px] font-bold shadow-lg rounded-full text-center w-[50px] h-[50px] lg:w-[60px] lg:h-[60px] border-4 border-[#f1f2f4] bg-[#fe7532] flex items-center justify-center">
                                     {isNaN(appData.daily_power - (appData.load * appData.grid_time))? 
                                     "0": 
                                     Math.round(((appData.daily_power - (appData.load * appData.grid_time)) + Number.EPSILON) * 100) / 100}kWp
                                 </div>
                             </div>
                             <div className="flex items-center w-[220px] m-auto justify-between my-1">
-                                <div className="bg-[#fe7532] text-[9px] shadow py-2 px-1 mr-1 rounded-full">Required Solar Battery Capacity</div>
+                                <div className="bg-[#fe7532] text-[9px] shadow font-bold py-2 px-1 mr-1 rounded-full">Required Solar Battery Capacity</div>
                                 <div 
-                                    className="text-[10px] shadow-lg rounded-full text-center w-[50px] h-[50px] border-4 border-[#f1f2f4] bg-[#fe7532] flex items-center justify-center">
+                                    className="text-[10px] sm:text-[12px] font-bold shadow-lg rounded-full text-center w-[50px] h-[50px] lg:w-[60px] lg:h-[60px] border-4 border-[#f1f2f4] bg-[#fe7532] flex items-center justify-center">
                                     {isNaN(appData.night_power)? "0" : appData.night_power}kWh
                                 </div>
                             </div>
                             <div className="flex items-center w-[220px] m-auto justify-between my-1">
-                                <div className="bg-[#fe7532] text-[9px] shadow py-2 px-1 mr-1 rounded-full">Maximum Load to be Powered</div>
+                                <div className="bg-[#fe7532] text-[9px] shadow py-2 font-bold px-1 mr-1 rounded-full">Maximum Load to be Powered</div>
                                 <div 
-                                    className="text-[10px] shadow-lg rounded-full text-center w-[50px] h-[50px] border-4 border-[#f1f2f4] bg-[#fe7532] flex items-center justify-center">
+                                    className="text-[10px] sm:text-[12px] font-bold shadow-lg rounded-full text-center w-[50px] h-[50px] lg:w-[60px] lg:h-[60px] border-4 border-[#f1f2f4] bg-[#fe7532] flex items-center justify-center">
                                     {isNaN(appData.load)? "0" : appData.load} kW
                                 </div>
                             </div>
@@ -274,7 +305,7 @@ function Calculator() {
                     <p className="text-base text-center text-[#1994cb] font-semibold mb-1">
                         Here is Your Most Suitable Solar System Option 
                     </p>
-                    <p className="text-[8px] bg-[#1994cb] p-1 text-center rounded-full shadow text-white">
+                    <p className="text-[8px] bg-[#1994cb] font-bold p-1 text-center rounded-full shadow text-white">
                         After reviewing your entry and need, here is a suitable solar system to cover all your energy needs.
                     </p>
                     <div className="p-3 border mt-2 border-green-700 bg-[#bbf7d0] rounded-lg">
@@ -315,25 +346,49 @@ function Calculator() {
                                         </div>
                                     </div>
                                 }
-                            
-                            
                             {
-                                recommendBattery(appData.night_power).length > 0 && recommendBattery(appData.night_power).map((x,i) => {
-                                    return (
-                                        <div key={i} className="rounded-lg flex flex-col w-[84px] items-center px-2 bg-green-700 min-h-5 text-white">
-                                            <div className="text-base text-center">
-                                            {
-                                                x[0]
-                                            }
-                                            </div>
-                                            <div className="text-[8px] text-center">
-                                                {
-                                                    x[1]
-                                                }
-                                            </div>
+                                console.log(recommendBattery(appData.night_power))
+                            }
+                            {
+                            recommendBattery(appData.night_power) && 
+                            <div className="flex flex-wrap justify-between items-center">
+                            
+                                <div className="rounded-lg flex flex-col w-[84px] items-center px-2 bg-green-700 min-h-5 text-white">
+                                    <div className="text-base text-center">
+                                    {
+                                        recommendBattery(appData.night_power)[0][0]
+                                    }
+                                    </div>
+                                    <div className="text-[8px] text-center">
+                                        {
+                                            recommendBattery(appData.night_power)[0][1]
+                                        }
+                                    </div>
+                                </div>
+                                {
+                                    recommendBattery(appData.night_power).length > 1 &&
+                                    <div className='text-xs font-bold'>
+                                        or
+                                    </div>
+                                }
+                                {
+                                    recommendBattery(appData.night_power).length > 1 &&
+                                    <div className="rounded-lg flex flex-col w-[84px] items-center px-2 bg-green-700 min-h-5 text-white">
+                                        <div className="text-base text-center">
+                                        {
+                                            recommendBattery(appData.night_power)[1][0]
+                                        }
                                         </div>
-                                    )
-                                })
+                                        <div className="text-[8px] text-center">
+                                            {
+                                                recommendBattery(appData.night_power)[1][1]
+                                            }
+                                        </div>
+                                    </div>
+                                }
+                                    
+                             
+                            </div>
                             }
                         </div>
                     </div>
